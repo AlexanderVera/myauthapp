e32b3cbe305df868cc42c4420c235a77db45c2a3
alexander.vera <alexander.vera@pragma.com.co>
2018-02-02 17:01:31 -0500

    publish online

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseConfig } from './firebase.config';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { routes } from './app.routes';
+import { AuthGuard } from './auth.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs/observable/from';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+  	AngularFireModule.initializeApp(FirebaseConfig.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    routes   
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+    firebase: {      
+        apiKey: 'AIzaSyAE_Un5yhEsWXuRuqLB5nvuJ8P3VmGmUF8',      
+        authDomain: 'ngchat-ab3a8.firebaseapp.com',      
+        databaseURL: 'https://ngchat-ab3a8.firebaseio.com',      
+        projectId: 'ngchat-ab3a8',      
+        storageBucket: '',   
+        messagingSenderId: '760180037976'    
+    }  
+};
